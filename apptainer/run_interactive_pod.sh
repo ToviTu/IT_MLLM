9e8e0f61033c2eed0160f5c42a9008d825841435
Zhuohao Ni <shuaiqidepeterni@gmail.com>
2024-07-11 18:33:12 -0500

    results for arc easy

--- apptainer/run_interactive_pod.sh
+++ apptainer/run_interactive_pod.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 
-. /home/research/jianhong.t/Instruction-tuned-Flamingo-MLLM/sh/set_environ_var.sh
+. /scratch/peterni/wustl/IT_MLLM/sh/set_environ_var.sh
 . ${WORKING_DIR}/sh/set_secrets.sh
 
 
 echo $HF_HOME
-singularity run --nv --bind /scratch,/storage1 /scratch/yu.zihao/lang-modeling.sif /bin/bash
+singularity run --nv --bind /scratch,/storage1 /scratch/peterni/lang-modeling.sif /bin/bash
+
