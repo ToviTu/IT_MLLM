bba623ad9ff433470c61c0f5b259bccecbeea3f6
tovitu <tovitusaikou@gmail.com>
2024-07-19 14:52:51 -0500

    fixed vizwiz template

--- apptainer/run_interactive_pod.sh
+++ apptainer/run_interactive_pod.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-. /scratch/peterni/wustl/IT_MLLM/sh/set_environ_var.sh
+. /home/research/jianhong.t/Instruction-tuned-Flamingo-MLLM/sh/set_environ_var.sh
 . ${WORKING_DIR}/sh/set_secrets.sh
 
 echo $HF_HOME
-singularity run --nv --bind /scratch,/storage1 /scratch/peterni/lang-modeling.sif /bin/bash
+singularity run --nv --bind /scratch,/storage1 /scratch/t.tovi/lang-modeling.sif /bin/bash
 
