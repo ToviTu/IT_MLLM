5c7abe65b3245f7ee4485388f48ef7b82631bae7
tovitu <tovitusaikou@gmail.com>
2024-07-19 14:52:54 -0500

    revoked environ variable changes

--- sh/set_environ_var.sh
+++ sh/set_environ_var.sh
@@ -1,11 +1,11 @@
 #! /bin/bash
 
-export MODEL_DIR="/storage1/chenguangwang/Active/vision_share"
-export STORAGE_DIR="/scratch/peterni/wustl/"
-export WORKING_DIR="/scratch/peterni/wustl/IT_MLLM/"
-export EVAL_DIR="/storage1/chenguangwang/Active/vision_share/dataset/eval"
-export PYTHONPATH="/scratch/peterni/wustl/IT_MLLM:$PYTHONPATH"
+#export MODEL_DIR="/storage1/chenguangwang/Active/vision_share"
+export STORAGE_DIR="/scratch/vision_share"
+export WORKING_DIR="/home/research/jianhong.t/Instruction-tuned-Flamingo-MLLM"
+#export EVAL_DIR="/storage1/chenguangwang/Active/vision_share/dataset/eval"
+#export PYTHONPATH="/scratch/peterni/wustl/IT_MLLM:$PYTHONPATH"
 
-export HF_DATASETS_CACHE="${MODEL_DIR}/datasets"
-export HF_HOME="${MODEL_DIR}/models"
-export TRANSFORMERS_CACHE="${MODEL_DIR}/models"
+export HF_DATASETS_CACHE="${STORAGE_DIR}/datasets"
+export HF_HOME="${STORAGE_DIR}/models"
+#export TRANSFORMERS_CACHE="${STORAGE_DIR}/models"
